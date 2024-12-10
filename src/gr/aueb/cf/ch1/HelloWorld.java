c08450c8fa2667b8049616479a5b3a75b55051f3
Katerina Savidaki <ksavvidaki@gmail.com>
2024-12-10 20:04:42 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello World in
+ * standard output (console)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");    // Prints Hello World
+    }
+}
