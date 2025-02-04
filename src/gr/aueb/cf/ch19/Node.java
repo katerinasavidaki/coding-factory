cbcda446d25d5d31cdc76f3a321b553815672667
Katerina Savidaki <ksavvidaki@gmail.com>
2025-02-04 17:54:37 +0200

    linked-lists created

---
+++ src/gr/aueb/cf/ch19/Node.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch19;
+
+/**
+ * Ειδικός κόμβος που περιορίζεται μόνο σε int
+ */
+public class Node {
+    private int item;
+
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+}
---
+++ src/gr/aueb/cf/ch19/NodeObj.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch19;
+
+/**
+ * Γενικός κόμβος
+ */
+public class NodeObj {
+    private Object item;
+
+    public Object getItem() {
+        return item;
+    }
+
+    public void setItem(Object item) {
+        this.item = item;
+    }
+}
